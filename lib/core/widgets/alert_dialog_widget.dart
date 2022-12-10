1cecca16cb0d777e80e853587727364c76b7b341
William Souza <Will051096@gmail.com>
2022-12-09 21:26:41 -0300

    validação textformfield

--- lib/core/widgets/alert_dialog_widget.dart
+++ lib/core/widgets/alert_dialog_widget.dart
@@ -12,76 +12,112 @@ class AlertDialogWidget extends StatefulWidget {
 }
 
 class _AlertDialogWidgetState extends State<AlertDialogWidget> {
+  final TextEditingController nomeParticipante = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String _infotext = "Informe o Nome";
+
+  void _resetar() {
+    nomeParticipante.text = '';
+    setState(() {
+      _infotext = "Informe o Nome";
+      _formKey = GlobalKey<FormState>();
+    });
+  }
+
+  void _nome() {
+    setState(() {
+      String? nome;
+      if (nome == nome?.isEmpty) {
+        _infotext = '$nome';
+      }
+      print(_infotext);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: context.colors.primary,
-      title: const Center(child: Text('Adicionar Nome')),
-      content: Container(
-        height: 700,
-        width: 500,
-        decoration: const BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(35)),
-        ),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.only(top: 25.0),
-              child: Text(
-                "N° 6",
-                style: TextStyle(fontSize: 50),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 50.0, bottom: 15),
-              child: SizedBox(
-                width: MediaQuery.of(context).size.width,
-                height: 100,
-                child: const Center(
-                    child: Text(
-                  'Nome da Pessoa ',
-                  style: TextStyle(fontSize: 30),
-                )),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: 100,
-              child: Column(
-                children: const [
-                  RadioListTileWidget(),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30.0),
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  labelText: 'Informe o nome',
-                  labelStyle: TextStyle(color: Colors.grey, fontSize: 25),
-                  border: OutlineInputBorder(),
-                  icon: Icon(Icons.person_add),
+    return Form(
+      key: _formKey,
+      child: AlertDialog(
+        backgroundColor: context.colors.primary,
+        title: const Center(child: Text('Adicionar Nome')),
+        content: Container(
+          height: 700,
+          width: 500,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(35)),
+          ),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 25.0),
+                child: Text(
+                  "N° 6",
+                  style: TextStyle(fontSize: 50),
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 50.0, bottom: 15),
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 100,
+                  child: Center(
+                      child: Text(
+                    _infotext,
+                    style: const TextStyle(fontSize: 30),
+                  )),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                child: Column(
+                  children: const [
+                    RadioListTileWidget(),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0),
+                child: TextFormField(
+                  controller: nomeParticipante,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "Informe o nome";
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Informe o nome',
+                    labelStyle: TextStyle(color: Colors.grey, fontSize: 25),
+                    border: OutlineInputBorder(),
+                    icon: Icon(Icons.person_add),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+        actions: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _nome();
+                  }
+                },
+                child: const Text("Adicionar"),
+              ),
+              MaterialButton(
+                onPressed: _resetar,
+                child: const Text("Excluir"),
+              ),
+            ],
+          ),
+        ],
       ),
-      actions: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MaterialButton(
-              onPressed: () {},
-              child: const Text("Adicionar"),
-            ),
-            MaterialButton(
-              onPressed: () {},
-              child: const Text("Excluir"),
-            ),
-          ],
-        ),
-      ],
     );
   }
 }
