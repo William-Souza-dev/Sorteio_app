a8418e9114c98623f924536a87a38bbe584084b9
William Souza <Will051096@gmail.com>
2023-01-07 20:32:49 -0300

    alteração na HomeView

--- lib/pages/home/view/home_view.dart
+++ lib/pages/home/view/home_view.dart
@@ -5,16 +5,16 @@ import 'package:sorteio_app/core/styles/colors_app.dart';
 class HomeView extends ChangeNotifier {
   void confirmed() {
     notifyListeners();
-    bool confirmed = false;
-    if (confirmed == true) {
+    int confirmed = 0;
+    if (confirmed == 0) {
       ColorsApp.i.secondary;
     }
   }
 
   void pending() {
     notifyListeners();
-    bool confirmed = false;
-    if (confirmed == true) {
+    int pending = 1;
+    if (pending == 1) {
       ColorsApp.i.quartuario;
     }
   }
